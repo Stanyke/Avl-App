afd5f83ace765054359185c784ef92457292307f
Stanyke <chibuike.stanley50@gmail.com>
2020-02-12 11:31:26 +0100

    Users can get all resturants open on a particular day based on their device day

--- routes/resturant.js
+++ routes/resturant.js
@@ -6,18 +6,120 @@ const client = require('../db/connectDB');
 //Routes Middlewares
 app.get('/', (req, res) =>
 {
-	client.query(`SELECT * FROM resturants`, (derr, myres) => 
-	{
-		if (derr)
-		{
-			res.status(500).send("We Encoutered An Error Getting Resturants");
-		}
+	const userCurrentDate = new Date();
+	const userCurrentDay = userCurrentDate.getDay();
 
-		if (myres.rows)
-        {
-			res.status(200).json({"resturants": myres.rows});
-		}
-	});
+	if (userCurrentDay === 0)
+	{
+		client.query(`SELECT * FROM resturants WHERE sunday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
+	
+	else if (userCurrentDay === 1)
+	{
+		client.query(`SELECT * FROM resturants WHERE monday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
+	
+	else if (userCurrentDay === 2)
+	{
+		client.query(`SELECT * FROM resturants WHERE tuesday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
+	
+	else if (userCurrentDay === 3)
+	{
+		client.query(`SELECT * FROM resturants WHERE wednesday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
+	
+	else if (userCurrentDay === 4)
+	{
+		client.query(`SELECT * FROM resturants WHERE thursday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
+	
+	else if (userCurrentDay === 5)
+	{
+		client.query(`SELECT * FROM resturants WHERE friday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
+	
+	else if (userCurrentDay === 6)
+	{
+		client.query(`SELECT * FROM resturants WHERE saturday != 'Closed'`, (derr, myres) => 
+		{
+			if (derr)
+			{
+				res.status(500).send("We Encoutered An Error Getting Resturants");
+			}
+
+			if (myres.rows)
+			{
+				res.status(200).json({"resturants_open_today": myres.rows});
+			}
+		});
+	}
 });
 
 const resturantRoute = app;
